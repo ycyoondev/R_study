9e3ed22daa0659ff7643fc5d89f1a9f00e440fd0
YCYoon2015 <z20152002@gmail.com>
2021-08-12 21:31:37 +0900

    210812 R study

---
+++ firstpjt/exam03.R
@@ -0,0 +1,18 @@
+
+v1 <- c(1,2,3,4)
+mat <- matrix(v1) # 기본은 열이다.
+
+mat <- matrix(v1, nrow = 2) # 행 지정정.
+mat
+
+mat <- matrix(v1, byrow = T) # 행 지정정.
+mat
+
+mat <- matrix(v1, nrow = 2) # 행 지정정.
+mat
+mat[2,1] # 행렬
+
+
+
+
+
