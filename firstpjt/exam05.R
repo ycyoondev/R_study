d346644d648d22a33ad67f5c88fdbcf86d929fb1
YCYoon2015 <z20152002@gmail.com>
2021-08-16 10:47:27 +0900

    210816 R study

---
+++ firstpjt/exam05.R
@@ -0,0 +1,38 @@
+# 데이터 프레임
+
+No <- c(1,2,3,4)
+Name <- c("Apple", "Peach", "Banana", "Grape")
+Price <- c(500, 200, 100, 50)
+Qty <- c(5,2,4,7)
+sales <- data.frame(No, Name, Price, Qty)
+sales
+
+View(sales)
+
+sales[ , 2]
+sales[,3]
+
+sales$Price
+sales$Name
+
+sales[c(1,2), ]
+
+subset(sales, Name == "Apple")
+subset(sales, Price >= 200)
+sales$Name
+subset(sales, Price >= 100, select=Name)
+
+subset(sales, Qty >= 2, select = -Price)
+subset(sales, Qty >= 3, select = c(Name, Price))
+
+
+
+
+
+
+
+
+
+
+
+
