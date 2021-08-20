1859b576fe7f4ed69134ec5a4b2ffd16e83a510c
YCYoon2015 <z20152002@gmail.com>
2021-08-20 21:00:41 +0900

    210820 R study

---
+++ firstpjt/exam09.R
@@ -0,0 +1,52 @@
+x <- c(1:4)
+y <- c("apple", "banana", "orange")
+xy <- c(x,y)
+xy[1]
+xy[1]+xy[2]
+xy[5:7]
+
+test1 = c(1, 2, 3, 4, 5)
+summary(test1)
+summary(Price)
+
+z=c(1:3, NA)
+
+z<-c(1:3, NA)
+
+
+is.na(z)
+c(1,1,1,2) == 2
+a <- c(3.14, pi, TRUE)
+pi
+a <- seq(1,10,1)
+a
+c <- 1:10
+c
+c(2, 4, 6, 8) + c(1, 3, 5, 7, 9)
+as.numeric(F)
+
+x <- c(1:3, NA)
+mean(x, na.rm=T)
+"+"(2,3)
+y=c(1,2,3,NA)
+3*y
+y*3
+sqrt(var(student$score))
+var(student$score)^(1/2)
+
+s <- c("monday", "tuesday", "wednesday")
+substr(s,1,2)
+
+
+
+
+x <- c(1:5)
+y <- seq(10,50,10)
+xy<-rbind(x,y)
+
+f <- function(x,a) return((x-a)^2)
+f(1:2,3)
+x <- c(1,2,3,NA)
+mean(x)
+x<-1:100
+sum(x>50)
